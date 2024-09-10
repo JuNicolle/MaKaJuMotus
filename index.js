ee98740b013a17ab09748f423387eade46e8a569
karine mullier <karine_mullier@hotmail.com>
2024-09-10 16:19:34 +0200

    longueur du mot

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 document.addEventListener('DOMContentLoaded',()=>{
     // on cree la liste de mots
     const listeMots=["toto","totomoto","maison"];
-    console.log(listeMots.length);
+    // console.log(listeMots.length);
     console.log(listeMots);
 // on calcule le nombre de mots
     let longueurListe=listeMots.length;
     console.log(longueurListe);
     // on tire au sort un mot
 
-    let nbAlea=Math.floor(Math.random()*longueurListe)+1;
+    let nbAlea=Math.floor(Math.random()*longueurListe);
     console.log(nbAlea);
     let motAtrouver = listeMots[nbAlea];
     console.log(motAtrouver.toString());
+
+    console.log(motAtrouver.length);
+    
 
 
 })
